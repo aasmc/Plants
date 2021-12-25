6bd3eb9c6309431027408657998f19302c1a8e9d
Alex Alexeyev <aasmc@yandex.ru>
2021-12-26 00:23:38 +0300

    Implement Injector

--- app/src/main/java/ru/aasmc/plants/di/Injector.kt
+++ app/src/main/java/ru/aasmc/plants/di/Injector.kt
@@ -1,2 +1,71 @@
 package ru.aasmc.plants.di
 
+import android.content.Context
+import androidx.annotation.VisibleForTesting
+import ru.aasmc.plants.data.PlantRepository
+import ru.aasmc.plants.data.cache.AppDatabase
+import ru.aasmc.plants.data.cache.PlantDao
+import ru.aasmc.plants.data.network.NetworkService
+import ru.aasmc.plants.ui.PlantListViewModelFactory
+
+interface ViewModelFactoryProvider {
+    fun providePlantListViewModelFactory(context: Context): PlantListViewModelFactory
+}
+
+val Injector: ViewModelFactoryProvider
+    get() = currentInjector
+
+private object DefaultViewModelProvider : ViewModelFactoryProvider {
+    override fun providePlantListViewModelFactory(context: Context): PlantListViewModelFactory {
+        return PlantListViewModelFactory(getPlantRepository(context))
+    }
+
+    private fun getPlantRepository(context: Context): PlantRepository {
+        return PlantRepository.getInstance(
+            plantDao(context),
+            plantService()
+        )
+    }
+
+    private fun plantService(): NetworkService = NetworkService()
+
+    private fun plantDao(context: Context): PlantDao =
+        AppDatabase.getInstance(context.applicationContext).plantDao()
+
+}
+
+private object Lock
+
+@Volatile
+private var currentInjector: ViewModelFactoryProvider =
+    DefaultViewModelProvider
+
+@VisibleForTesting
+private fun setInjectorForTesting(injector: ViewModelFactoryProvider?) {
+    synchronized(Lock) {
+        currentInjector = injector ?: DefaultViewModelProvider
+    }
+}
+
+@VisibleForTesting
+private fun resetInjector() =
+    setInjectorForTesting(null)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/ru/aasmc/plants/ui/PlantListViewModel.kt
+++ app/src/main/java/ru/aasmc/plants/ui/PlantListViewModel.kt
@@ -97,8 +97,15 @@ class PlantListViewModel internal constructor(
     }
 }
 
+class PlantListViewModelFactory(
+    private val repository: PlantRepository
+) : ViewModelProvider.NewInstanceFactory() {
 
-
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return PlantListViewModel(repository) as T
+    }
+}
 
 
 
